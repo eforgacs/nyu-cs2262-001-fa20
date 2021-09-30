4f0a9a45028c2afd68180533587ee5604b295e1a
Edward Forgacs <eddie.forgacs@gmail.com>
2021-09-30 13:23:14 -0400

    Update run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,16 @@
+from datetime import datetime
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/time')
 def hello_world():
-    return 'Hello world!'
+    now = datetime.now()
+    return now.strftime("%H:%M:%S")
 
 
-app.run(host='0.0.0.0',
+app.run(host='127.0.0.1',
         port=8080,
         debug=True)
